238b2b52075979aa1c4c4715d087e81f7f3e8498
AryanVanupratap <aryanvanupratap@gmail.com>
2024-09-02 15:35:02 +0000

    js

--- avpssj/basics.js
+++ avpssj/basics.js
@@ -1,1 +1,21 @@
-console.log("aryan");
+const accountID = 69;
+let accountemail = "aryan@google.com";
+var accountpassword = 12345;
+accountCity = "Bhubaneshwar";
+// accountID = 2; // not allowed
+accountemail = "hitesh@google.com";
+console.log(accountemail);
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountID, accountemail, accountpassword, accountCity]);
+"use strict"; // treat all js code as newer version
+// alert("hello"); we are using node.js not browser
+// we are using node.js not browser
+// alert(3+3);
+let name= "aryan"
+let age = 18;
+let isLoggedIn = false;
+console.log(typeof null); // object
+console.log(typeof undefined) // undefined
